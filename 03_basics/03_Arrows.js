73b3edc0354ad1af0af20ea56cc451c31b172058
VISHWAS RAJ JADON <vrjadon3@gmail.com>
2024-06-10 16:24:04 +0530

    learnings

--- 03_basics/03_Arrows.js
+++ 03_basics/03_Arrows.js
@@ -4,10 +4,11 @@ const user = {
     age : 18,
     welcomeMsg : function(){
         console.log(`${this.userName}, Welcome to our site`);  //to access current context, use 'this' keyword
-
+        console.log(this);
     }
 }
-console.log("noob");
+
 user.welcomeMsg();  //Vishwas, Welcome to our site
 user.userName = "Rinku"
 user.welcomeMsg();
+console.log(this);
--- 03_basics/03_arrows.js
+++ 03_basics/03_arrows.js
@@ -1,11 +1,13 @@
 
 const user = {
-    name : "vishwas",
+    userName : "Vishwas",
     age : 18,
     welcomeMsg : function(){
-        console.log(`${this.name}, Welcome to our site`);  //to access current context, use 'this' keyword
+        console.log(`${this.userName}, Welcome to our site`);  //to access current context, use 'this' keyword
 
     }
 }
-
+console.log("noob");
+user.welcomeMsg();  //Vishwas, Welcome to our site
+user.userName = "Rinku"
 user.welcomeMsg();
